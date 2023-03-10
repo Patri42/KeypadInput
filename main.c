f404bf7d2e4c9a6e98a52dcc28a66286e82252d7
Patri42 <patrick.borzych@gmail.com>
2023-03-10 09:52:47 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define PIN_LENGTH 4
+#define MAX_ATTEMPTS 3
+
+// Define the correct PIN
+const char* correct_pin = "1234";
+
+// Define the keypad matrix
+const char keypad[4][4] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+};
+
+// Function to read a key from the keypad
+char read_key() {
+    char key = '\0';
+    // Code to read the key from the keypad
+    // (This will depend on the specific keypad hardware you are using)
+    return key;
+}
+
+int main() {
+    // Initialize variables
+    char pin[PIN_LENGTH + 1] = {0};
+    int attempts = 0;
+    bool success = false;
+    
+    // Loop until the correct PIN is entered or the maximum attempts are reached
+    while (!success && attempts < MAX_ATTEMPTS) {
+        // Read the first digit of the PIN
+        printf("Enter digit %d of %d: ", 1, PIN_LENGTH);
+        pin[0] = read_key();
+        printf("%c\n", pin[0]);
+        
+        // Read the remaining digits of the PIN
+        for (int i = 1; i < PIN_LENGTH; i++) {
+            printf("Enter digit %d of %d: ", i+1, PIN_LENGTH);
+            pin[i] = read_key();
+            printf("%c\n", pin[i]);
+        }
+        
+        // Check if the PIN is correct
+        if (strcmp(pin, correct_pin) == 0) {
+            printf("PIN correct!\n");
+            success = true;
+        } else {
+            printf("Incorrect PIN.\n");
+            attempts++;
+        }
+    }
+    
+    // If the correct PIN was entered, turn on the LED
+    if (success) {
+        // Code to turn on the LED
+    }
+    
+    // If the maximum attempts were reached, lock the door or turn off the LED
+    if (!success && attempts >= MAX_ATTEMPTS) {
+        // Code to lock the door or turn off the LED
+    }
+    
+    return 0;
+}
